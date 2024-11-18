18e5cbe483f191c6fcba2a8805e79acb46d2983c
Chris Schaaf <cschaaf@unomaha.edu>
2024-11-18 15:39:16 -0600

    adding some more features so its not just hello world alone

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -2,8 +2,22 @@ function Greeting({ name }) {
   return <h1>Hello, {name}</h1>;
 }
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
 export default function App() {
-  return <Greeting name="world" />
+  return (
+    <div>
+      <h1>Welcome to my app!</h1>
+      <Greeting name="world" />
+      <MyButton />
+    </div>
+  );
 }
 
 
