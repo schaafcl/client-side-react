271a8af202452712309d8830b2d0f24a799b87a9
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-01 09:02:11 -0600

    final commit before submitting assignemnt

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -21,7 +21,27 @@ function AboutPage() {
   );
 }
 
+function ShoppingList() {
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  )
 
+}
 
 export default function App() {
   return (
@@ -30,8 +50,7 @@ export default function App() {
       <Greeting name="world" />
       <MyButton />
       <AboutPage />
-    
-      
+      <ShoppingList />    
       </>
   );
 }
