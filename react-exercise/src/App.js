aa35c43def58f50df3c34abec1fed4e50b944b0b
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-01 08:55:38 -0600

    adding more features

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -10,14 +10,33 @@ function MyButton() {
   );
 }
 
+// markup with JSX
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+
+  );
+}
+
+
+
 export default function App() {
   return (
-    <div>
+    <>
       <h1>Welcome to my app!</h1>
       <Greeting name="world" />
       <MyButton />
-    </div>
+      <AboutPage />
+    
+      
+      </>
   );
 }
+
+
+
 
 
